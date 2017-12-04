11f252941f32f5ffdb70ce2ac16a633ebcbe3da9
by <baiyang1114@163.com>
2017-12-04 17:31:32 +0800

    Del useless file

--- style/one/js/d/slide_12.ascx
+++
@@ -1,6 +0,0 @@
-﻿<% @ Control  Language="C#" Inherits="PageAdmin.patag"%>
-<% @ Import NameSpace="System.Data"%>
-<% @ Import NameSpace="System.Data.OleDb"%>
-<%Start();%>
-
-<%End();%>
--- style/three/js/d/link_1.js
+++
@@ -1,2 +0,0 @@
-﻿
-document.write("<select onChange=Link_Open(this.options[this.selectedIndex].value,'link')><option value=''>fsdf</option><option value='http://www.baidu.com'>sdf</option></select>");
